59ea288a8770731b9e34805dea9970adce9a38b5
Carlos García Pérez <carlosziok@gmail.com>
2024-01-28 01:33:20 +0300

    Logic created

--- SemaforWithButton/ViewController.swift
+++ SemaforWithButton/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet weak var redSemafor: UIView!
+    
+    @IBOutlet weak var yellowSemafor: UIView!
+    
+    @IBOutlet weak var greenSemafor: UIView!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    enum SemaforColor {
+            case red, yellow, green
+        }
+        
+        var currentColor: SemaforColor = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func semaforButtonDidTapped(_ sender: UIButton) {
+        redSemafor.alpha = 0.3
+        yellowSemafor.alpha = 0.3
+        greenSemafor.alpha = 0.3
+        switch currentColor {
+            case .red:
+                redSemafor.alpha = 1
+                button.setTitle("NEXT", for: .normal)
+                currentColor = .yellow
+            case .yellow:
+                yellowSemafor.alpha = 1
+                currentColor = .green
+            case .green:
+                greenSemafor.alpha = 1
+                currentColor = .red
+        }
+    }
 }
+
 
